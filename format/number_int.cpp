a25677ec4ea571f1a8c5ad4e1603f857e51007fe
RusProgger <ruslantimka92@gmail.com>
2026-08-26 19:28:09 +0300

    Add work number in C++23

--- format/number_int.cpp
+++ format/number_int.cpp
@@ -12,4 +12,13 @@ int main() {
     std::cout << std::format("Hex: {:x}\n", numb);
     std::cout << std::format("HexUpper: {:X}\n", numb);
     std::cout << std::format("Bin: {:b}\n", numb);
+
+    // time 
+
+    int hour{};
+    int min{};
+
+    std::cin >> hour;
+    std::cin >> min;
+    std::cout << std::format("Time: {:02}:{:02}\n", hour, min);
 }
