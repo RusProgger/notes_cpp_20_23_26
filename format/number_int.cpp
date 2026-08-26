bd6f423b3755fcfa8d1761317830a6557692beb8
RusProgger <ruslantimka92@gmail.com>
2026-08-26 19:10:59 +0300

    Add work number in C++23

---
+++ format/number_int.cpp
@@ -0,0 +1,15 @@
+#include <format>
+#include <iostream>
+
+int main() {
+
+    int numb{};
+    std::cout << std::format("Input number: ");
+    std::cin >> numb; 
+
+
+    std::cout << std::format("Dec: {:d}\n", numb);
+    std::cout << std::format("Hex: {:x}\n", numb);
+    std::cout << std::format("HexUpper: {:X}\n", numb);
+    std::cout << std::format("Bin: {:b}\n", numb);
+}
