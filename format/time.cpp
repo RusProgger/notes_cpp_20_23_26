c3b5724d4951522959dbf979c14ec8efa43cb08a
RusProgger <ruslantimka92@gmail.com>
2026-08-27 22:07:46 +0300

    Add time

--- format/time.cpp
+++ format/time.cpp
@@ -8,4 +8,16 @@ int main() {
 
     std::cout << std::format("Enter hour: ");
     std::cin >> hour;
+
+    std::cout << std::format("Enter minute: ");
+    std::cin >> minute;
+
+    if(hour > 24 || hour < 0 && minute > 59 || minute < 0) {
+        std::cout << std::format("Error! Hours cannot be greater than 24 or less than 0.");
+        exit(0);
+    } else {
+        std::cout << std::format("Time: {:02} : {:02}", hour, minute);
+    }
+
+    return 0;
 }
