4245a336501c69da98fbc682a09f339ef48eb3b8
RusProgger <ruslantimka92@gmail.com>
2026-08-29 15:25:05 +0300

    Add function lambda

--- function/fun.cpp
+++ function/fun.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <format>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
+
+    std::cout << std::format("Add: ") << add(10, 15);
+
+
+    // LAMBDA FUNCTION
+
+    auto lambda = [](int a, int b) {
+        return a * b;
+    };
+
+    int res =  {lambda(5, 4)};
+
+    std::cout << std::format("\nLambda: ") << res;
 
     return 0;
 }
