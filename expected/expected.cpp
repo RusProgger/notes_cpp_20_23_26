a5174e739b5047350acc07ff30fe849169fe739e
RusProgger <ruslantimka92@gmail.com>
2026-08-31 01:32:11 +0300

    Add expected

--- expected/expected.cpp
+++ expected/expected.cpp
@@ -1,5 +1,15 @@
 #include <print>
 #include <expected>
+
+std::expected<int, std::string>Divide(int a, int b) {
+    if(b == 0) {
+        return std::unexpected("Divide by zero!!!\n");
+    }
+
+    result a / b;
+}
+
+
 int main {
 
     auto result = Divide(10, 2);
